2f3177dce513337d6b0e791f72beb7f4fec75c26
Just another Dev <60642132+prohzgod@users.noreply.github.com>
2022-02-19 12:31:31 +0700

    add something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./components/TodoList";
+import Textfield from "@atlaskit/textfield";
+import Button from "@atlaskit/button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>Danh sách cần làm</h3>
+      <Textfield
+        name="add-todo"
+        placeholder="Thêm việc cần làm..."
+        elemAfterInput={
+          <Button isDisabled={true} appearance="primary">
+            Thêm
+          </Button>
+        }
+        css={{ padding: "2px 4px 2px" }}
+      ></Textfield>
+      <TodoList />
+    </>
   );
 }
 
